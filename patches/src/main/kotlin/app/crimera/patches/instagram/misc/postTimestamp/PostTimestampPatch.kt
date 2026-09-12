5972c95636109b63deab6930ecc6de5a3fba0a97
dave.palt <git@dav3.cc>
2026-09-13 01:33:36 +0200

    fix: missing USER_SESSION_CLASS import

--- patches/src/main/kotlin/app/crimera/patches/instagram/misc/postTimestamp/PostTimestampPatch.kt
+++ patches/src/main/kotlin/app/crimera/patches/instagram/misc/postTimestamp/PostTimestampPatch.kt
@@ -9,6 +9,7 @@ package app.crimera.patches.instagram.misc.postTimestamp
 import app.crimera.patches.instagram.misc.settings.settingsPatch
 import app.crimera.patches.instagram.utils.Constants.COMPATIBILITY_INSTAGRAM
 import app.crimera.patches.instagram.utils.Constants.PREF_CALL_DESCRIPTOR
+import app.crimera.patches.instagram.utils.Constants.USER_SESSION_CLASS
 import app.crimera.patches.instagram.utils.enableSettings
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
